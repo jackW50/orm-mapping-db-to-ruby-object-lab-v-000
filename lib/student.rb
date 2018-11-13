1b737ed75d12dbc5952168f71098aaafabd78bb2
John Werenski <jtwerenski@gmail.com>
2018-11-13 14:38:27 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -42,7 +42,15 @@ class Student
   end 
   
   def self.students_below_12th_grade
-    all.select {|student| student.grade < "12"}
+    sql =<<-SQL
+        SELECT * 
+        FROM students 
+        WHERE students.grade < 12 
+    SQL
+    
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end 
   end 
   
   def self.first_X_students_in_grade_10(num)
